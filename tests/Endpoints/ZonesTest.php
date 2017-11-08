af9fd959c14249af4f93967a25475648fb3e9d54
Tyler MeekLogic Vigario <tylervigario90@gmail.com>
2017-11-07 20:23:40 -0800

    Another attempt

--- tests/Endpoints/ZonesTest.php
+++ tests/Endpoints/ZonesTest.php
@@ -147,8 +147,8 @@ class ZonesTest extends TestCase
         $zones = new \Cloudflare\API\Endpoints\Zones($mock);
         $analytics = $zones->getAnalyticsDashboard("c2547eb745079dac9320b638f5e225cf483cc5cfdda41");
 
-        $this->assertNotEmpty($analytics->totals->since);
-        $this->assertNotEmpty($analytics->timeseries[0]->since);
+        $this->assertObjectHasAttribute("since", $analytics->totals);
+        $this->assertObjectHasAttribute("since", $analytics->timeseries[0]);
     }
     
     public function testChangeDevelopmentMode()
