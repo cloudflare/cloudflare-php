b15b4ecc5157019b38887b93914acc103487c94a
Kleis Auke Wolthuizen <github@kleisauke.nl>
2017-11-21 17:49:29 +0100

    Class autoloading correctness

--- tests/Configurations/ConfigurationsZoneLockdownTest.php
+++ tests/Configurations/ConfigurationsZoneLockdownTest.php
@@ -6,7 +6,7 @@
  * Date: 05/09/2017
  * Time: 13:50
  */
-class ConfigurationZoneLockdownTest extends TestCase
+class ConfigurationsZoneLockdownTest extends TestCase
 {
     public function testGetArray()
     {
