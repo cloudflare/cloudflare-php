d895b50e47349850f1b25933fc75d240fac44e45
dave <dave@zivitech.com>
2019-08-12 14:25:33 +0300

    add endpoint for server-side exclude setting

--- src/Endpoints/ZoneSettings.php
+++ src/Endpoints/ZoneSettings.php
@@ -9,9 +9,12 @@
 namespace Cloudflare\API\Endpoints;
 
 use Cloudflare\API\Adapter\Adapter;
+use Cloudflare\API\Traits\BodyAccessorTrait;
 
 class ZoneSettings implements API
 {
+    use BodyAccessorTrait;
+
     private $adapter;
 
     public function __construct(Adapter $adapter)
@@ -65,6 +68,20 @@ class ZoneSettings implements API
     {
         $return = $this->adapter->get(
             'zones/' . $zoneID . '/settings/email_obfuscation'
+        );
+        $body   = json_decode($return->getBody());
+
+        if ($body->success) {
+            return $body->result->value;
+        }
+
+        return false;
+    }
+
+    public function getServerSideExcludeSetting($zoneID)
+    {
+        $return = $this->adapter->get(
+            'zones/' . $zoneID . '/settings/server_side_exclude'
         );
         $body   = json_decode($return->getBody());
 
@@ -173,6 +190,23 @@ class ZoneSettings implements API
 
         if ($body->success) {
             return true;
+        }
+
+        return false;
+    }
+
+    public function updateServerSideExcludeSetting($zoneID, $value)
+    {
+        $return = $this->adapter->patch(
+            'zones/' . $zoneID . '/settings/server_side_exclude',
+            [
+                'value' => $value
+            ]
+        );
+        $body = json_decode($return->getBody());
+
+        if ($body->success) {
+            return $body->result->value;
         }
 
         return false;
