8c862ed433e49fe1495e0fb1700a51ebb1ce87da
Phil Young <philip.young@ukfast.co.uk>
2021-09-30 16:47:24 +0100

    Add Account Member Creation
    We can now create account members threough the SDK

---
+++ tests/Endpoints/AccountMembersTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Cloudflare\API\Adapter\Adapter;
+use Cloudflare\API\Endpoints\AccountMembers;
+
+class AccountMembersTest extends TestCase
+{
+    public function testAddAccountMember(): void
+    {
+        $response = $this->getPsr7JsonResponseForFixture('Endpoints/createAccountMember.json');
+
+        $mock = $this->getMockBuilder(Adapter::class)->getMock();
+        $mock->method('post')->willReturn($response);
+
+        $mock->expects($this->once())
+            ->method('post')
+            ->with(
+                $this->equalTo('accounts/01a7362d577a6c3019a474fd6f485823/members'),
+                $this->equalTo([
+                    'email' => 'user@example.com',
+                    'roles' => [
+                        '3536bcfad5faccb999b47003c79917fb',
+                    ],
+                ])
+            );
+
+        $accountMembers = new AccountMembers($mock);
+        $accountMembers->addAccountMember('01a7362d577a6c3019a474fd6f485823', 'user@example.com', ['3536bcfad5faccb999b47003c79917fb']);
+
+        $this->assertEquals('4536bcfad5faccb111b47003c79917fa', $accountMembers->getBody()->result->id);
+    }
+}
