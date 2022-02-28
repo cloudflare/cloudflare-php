158af5209a4ac89322ff1bdb1093b3003beb381d
Michal Korotkiewicz <michal.korotkiewicz@smartframe.io>
2022-02-28 15:35:38 +0100

    try to pass action on php 7.1 - 8.1

--- src/Endpoints/KeyValue.php
+++ src/Endpoints/KeyValue.php
@@ -36,13 +36,13 @@ class KeyValue implements API
         ];
     }
 
-    public function getKeyValue(string $accountId, string $namespaceId, string $key)
+    public function getKeyValue(string $accountId, string $namespaceId, string $key): ?string
     {
         $uri = sprintf('accounts/%s/storage/kv/namespaces/%s/values/%s', $accountId, $namespaceId, $key);
         return $this->request('get', $uri)->result;
     }
 
-    public function getKeyMetadata(string $accountId, string $namespaceId, string $key)
+    public function getKeyMetadata(string $accountId, string $namespaceId, string $key): ?stdClass
     {
         $uri = sprintf('accounts/%s/storage/kv/namespaces/%s/metadata/%s', $accountId, $namespaceId, $key);
         return $this->request('get', $uri)->result;
