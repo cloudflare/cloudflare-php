1b150b9d1255bb35b9f3bdd43de02671d1bfa520
Mahdi Akbari <67573454+MAAK-2904@users.noreply.github.com>
2023-06-16 20:59:15 +0330

    Authorization Param Added To Headers
    Authorization param added to headers for solving Invalid request headers

--- src/Auth/APIKey.php
+++ src/Auth/APIKey.php
@@ -11,18 +11,21 @@ class APIKey implements Auth
 {
     private $email;
     private $apiKey;
+    private $authorization;
 
-    public function __construct(string $email, string $apiKey)
+    public function __construct(string $email, string $apiKey, string $authorization)
     {
         $this->email  = $email;
         $this->apiKey = $apiKey;
+        $this->authorization = $authorization;
     }
 
     public function getHeaders(): array
     {
         return [
             'X-Auth-Email'   => $this->email,
-            'X-Auth-Key' => $this->apiKey
+            'X-Auth-Key' => $this->apiKey,
+            'Authorization' => $this->authorization
         ];
     }
 }
