2d3f198773e865b5de2357d7bdbc52bdf42e8f97
Jacob Fowler <6750990+jafowler@users.noreply.github.com>
2024-12-17 18:18:20 -0500

    make cache purge environment aware (#267)

--- tests/Endpoints/ZoneCacheTest.php
+++ tests/Endpoints/ZoneCacheTest.php
@@ -61,9 +61,10 @@ class ZoneCacheTest extends TestCase
             ->method('post')
             ->with(
                 $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/purge_cache'),
-                $this->equalTo(['files' => [
-                    'https://example.com/file.jpg',
-                ]
+                $this->equalTo([
+                    'files' => [
+                        'https://example.com/file.jpg',
+                    ]
                 ])
             );
 
@@ -71,6 +72,108 @@ class ZoneCacheTest extends TestCase
         $result = $zones->cachePurge('c2547eb745079dac9320b638f5e225cf483cc5cfdda41', [
             'https://example.com/file.jpg',
         ]);
+
+        $this->assertTrue($result);
+        $this->assertEquals('023e105f4ecef8ad9ca31a8372d0c353', $zones->getBody()->result->id);
+    }
+
+    public function testCachePurgeIncludingEnvironments()
+    {
+        $envResp = $this->getPsr7JsonResponseForFixture('Endpoints/getEnvironments.json');
+        $cacheResp = $this->getPsr7JsonResponseForFixture('Endpoints/cachePurge.json');
+        $mock = $this->getMockBuilder(\Cloudflare\API\Adapter\Adapter::class)->getMock();
+
+        $mock->expects($this->once())
+            ->method('get')
+            ->willReturn($envResp)
+            ->with(
+                $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/environments')
+            );
+
+        $mock->expects($this->exactly(4))
+            ->method('post')
+            ->willReturn($cacheResp)
+            ->withConsecutive(
+                [
+                    $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/environments/first/purge_cache'),
+                    $this->equalTo([
+                        'files' => [
+                            'https://example.com/file.jpg',
+                        ]
+                    ])
+                ],
+                [
+                    $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/environments/second/purge_cache'),
+                    $this->equalTo([
+                        'files' => [
+                            'https://example.com/file.jpg',
+                        ]
+                    ])
+                ],
+                [
+                    $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/environments/third/purge_cache'),
+                    $this->equalTo([
+                        'files' => [
+                            'https://example.com/file.jpg',
+                        ]
+                    ])
+                ],
+                [
+                    $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/purge_cache'),
+                    $this->equalTo([
+                        'files' => [
+                            'https://example.com/file.jpg',
+                        ]
+                    ])
+                ]
+            );
+
+        $zones = new \Cloudflare\API\Endpoints\Zones($mock);
+        $result = $zones->cachePurge('c2547eb745079dac9320b638f5e225cf483cc5cfdda41', [
+            'https://example.com/file.jpg',
+        ], null, null, true);
+
+        $this->assertTrue($result);
+        $this->assertEquals('023e105f4ecef8ad9ca31a8372d0c353', $zones->getBody()->result->id);
+    }
+
+    public function testCachePurgeEverythingIncludingEnvironments()
+    {
+        $envResp = $this->getPsr7JsonResponseForFixture('Endpoints/getEnvironments.json');
+        $cacheResp = $this->getPsr7JsonResponseForFixture('Endpoints/cachePurgeEverything.json');
+        $mock = $this->getMockBuilder(\Cloudflare\API\Adapter\Adapter::class)->getMock();
+
+        $mock->expects($this->once())
+            ->method('get')
+            ->willReturn($envResp)
+            ->with(
+                $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/environments')
+            );
+
+        $mock->expects($this->exactly(4))
+            ->method('post')
+            ->willReturn($cacheResp)
+            ->withConsecutive(
+                [
+                    $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/environments/first/purge_cache'),
+                    $this->equalTo(['purge_everything' => true])
+                ],
+                [
+                    $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/environments/second/purge_cache'),
+                    $this->equalTo(['purge_everything' => true])
+                ],
+                [
+                    $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/environments/third/purge_cache'),
+                    $this->equalTo(['purge_everything' => true])
+                ],
+                [
+                    $this->equalTo('zones/c2547eb745079dac9320b638f5e225cf483cc5cfdda41/purge_cache'),
+                    $this->equalTo(['purge_everything' => true])
+                ]
+            );
+
+        $zones = new \Cloudflare\API\Endpoints\Zones($mock);
+        $result = $zones->cachePurgeEverything('c2547eb745079dac9320b638f5e225cf483cc5cfdda41', true);
 
         $this->assertTrue($result);
         $this->assertEquals('023e105f4ecef8ad9ca31a8372d0c353', $zones->getBody()->result->id);
